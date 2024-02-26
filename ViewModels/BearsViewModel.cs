d4c2d0a33cf3b4404d7d58af2db11476a0681019
User <User@LAB2-7>
2024-02-26 14:09:58 +0200

    לא עובד

---
+++ ViewModels/BearsViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShellLessonStep2.Models;
+using ShellLessonStep2.Services;
+
+namespace ShellLessonStep2.ViewModels
+{
+    public class BearsViewModel:ViewModelBase
+    {
+        //public BearsViewModel()
+        //{
+        //    animals = new ObservableCollection<Animal>();
+        //}
+        //private ObservableCollection<Animal> animals;
+        //public ObservableCollection<Animal> Animals
+        //{
+        //    get
+        //    {
+        //        return this.animals;
+        //    }
+        //    set
+        //    {
+        //        this.animals = value;
+        //        OnPropertyChanged();
+        //    }
+        //}
+    }
+}
---
+++ ViewModels/CatsViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShellLessonStep2.Models;
+using ShellLessonStep2.Services;
+using Xamarin.Google.Crypto.Tink.Mac;
+
+namespace ShellLessonStep2.ViewModels
+{
+    public class CatsViewModel:ViewModelBase
+    {
+        public CatsViewModel()
+        {
+            animals = new ObservableCollection<Animal>();
+        }
+        private ObservableCollection<Animal> animals;
+        public ObservableCollection<Animal> Animals
+        {
+            get
+            {
+                return this.animals;
+            }
+            set
+            {
+                this.animals = value;
+                OnPropertyChanged();
+            }
+        }
+      
+    }
+}
